6fb650f9646012036e1d3e7dd94550f4d213cd9b
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2025-12-28 23:16:24 +0530

    Initial l1 event listener

--- src/l1/listener.rs
+++ src/l1/listener.rs
@@ -1,1 +1,16 @@
-// L1 event listener
+use crate::config::L1Config;
+
+pub struct L1Listener {
+    config: L1Config,
+}
+
+impl L1Listener {
+    pub fn new(config: L1Config) -> Self {
+        Self { config }
+    }
+    
+    pub async fn start(&self) -> anyhow::Result<()> {
+        // TODO: Connect to L1 and listen for events
+        Ok(())
+    }
+}
