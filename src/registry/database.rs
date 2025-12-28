a1e6432043c04d7e023e9624d3865df8a1f4accf
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2025-12-28 23:18:45 +0530

    Batch data saving

--- src/registry/database.rs
+++ src/registry/database.rs
@@ -1,1 +1,16 @@
-// Store batch metadata
+use crate::BatchMetadata;
+
+pub struct Registry {
+    // TODO: Add database connection
+}
+
+impl Registry {
+    pub fn new() -> Self {
+        Self {}
+    }
+    
+    pub async fn store(&self, metadata: BatchMetadata) -> anyhow::Result<()> {
+        // TODO: Store to database
+        Ok(())
+    }
+}
